1ce04b7ad6bd50a0b23e19c7e699e021972aaf6e
Tanguy Genthon <72631088+TanguyGen@users.noreply.github.com>
2025-04-09 09:09:12 +0200

    Comment and make sure the image is replaced by a dot when the image does not exist

--- R/app_server.R
+++ R/app_server.R
@@ -10,52 +10,55 @@
 app_server <- function(input, output, session) {
   options(shiny.maxRequestSize=1000*1024^2)
   
-  Info_table<-read.csv(system.file("app/www","Info_table.csv", package = 'RCaNExplorer'))
+  Info_table<-read.csv(system.file("app/www","Info_table.csv", package = 'RCaNExplorer')) #Import the metadata associated to each species (Color, FullName, Representation of the biomass?)
   
-  data <- reactiveValues(CaNSample = NULL, CaNSample_long = NULL, numgroups = NULL)
+  data <- reactiveValues(CaNSample = NULL, CaNSample_long = NULL) #Create a reactive value that will contain the data
   
   observe({
-    if (!is.null(input$rcanfile) && length(input$rcanfile$datapath) > 0) {
+    if (!is.null(input$rcanfile) && length(input$rcanfile$datapath) > 0) { #If a file was downloaded
+      
+      #Empty the reactive value
       data$CaNSample <- NULL
       data$CaNSample_long <- NULL
-      gc()
       
-      e <- new.env()
-      load(input$rcanfile$datapath, envir = e)
-      loaded_objs <- ls(envir = e)
+      e <- new.env()  #Create a new environment
+      load(input$rcanfile$datapath, envir = e)       #Load the file in the new environment
+      loaded_objs <- ls(envir = e) # Get all the variables from the environment
       
       if (length(loaded_objs) != 1) {
         showNotification("Please upload an RData file with only one object.", type = "error")
         return()
       }
       
-      obj <- e[[loaded_objs[[1]]]]
+      obj <- e[[loaded_objs[[1]]]] #Obj=CaNSample
       
-    } else if (is.null(data$CaNSample)) {
-      e <- new.env()
-      if (!file.exists("Data/CaNSample20240306.Rdata")) {
-        load(file = url("https://ftp.nmdc.no/nmdc/IMR/modeldata/CaNSample20240306.Rdata"), envir = e)
-      } else {
-        load("Data/CaNSample20240306.Rdata", envir = e)
-      }
-      loaded_objs <- ls(envir = e)
+    } else if (is.null(data$CaNSample)) { #If no RCaN file in input and no foodweb already downloaded, input a standard food web
+      e <- new.env()#Create a new environment
       
+      if (!file.exists("Data/CaNSample20240306.Rdata")) { #If the file does not exist, load it from its repository
+          load(file = url("https://ftp.nmdc.no/nmdc/IMR/modeldata/CaNSample20240306.Rdata"), envir = e)
+        } else { #else, load the existing file
+          load("Data/CaNSample20240306.Rdata", envir = e)
+        }
+      loaded_objs <- ls(envir = e) # Get all the variables from the environment
+      
+      #Check that there is only 1 object in the environment
       if (length(loaded_objs) != 1) {
         showNotification("Default RData must contain only one object.", type = "error")
         return()
       }
       
-      obj <- e[[loaded_objs[[1]]]]
+      obj <- e[[loaded_objs[[1]]]] #Obj=CaNSample
       
     } else {
       return()
     }
     
-    data$CaNSample <- obj
-    data$CaNSample_long <- transform_CaNSample(data$CaNSample)
+    data$CaNSample <- obj #Put the loaded object into the reactive value
+    data$CaNSample_long <- transform_CaNSample(data$CaNSample) #Create CaNSample_long, a 3 columns version of CaNSample to simplify further handling
   })
   
-  transform_CaNSample <- function(CaNSample) {
+  transform_CaNSample <- function(CaNSample) { #Function to create CaNSample_long
     tibble::as_tibble(as.matrix(CaNSample$mcmc)) %>%
       mutate(Sample_id = 1:nrow(as.matrix(CaNSample$mcmc))) %>%
       pivot_longer(
@@ -66,31 +69,26 @@ app_server <- function(input, output, session) {
       )
   }
   
-  Positions <- reactiveValues(x = NULL, y = NULL)
-  
-  observe({
-    Positions$x <- data$CaNSample$CaNmod$components_param$X
-    Positions$y <- data$CaNSample$CaNmod$components_param$Y
-  })
-  
-  observeEvent(input$node_positions, {
-    Positions$x <- sapply(input$node_positions, `[[`, "x") / 1000
-    Positions$y <- sapply(input$node_positions, `[[`, "y") / 1000
-  })
-  
+  #Render the interactive foodweb
   output$Foodweb <- visNetwork::renderVisNetwork({
-    req(data$CaNSample)
+    req(data$CaNSample)  #Make sure we have data
     
-    list_element <- data$CaNSample$CaNmod$components_param$Component
-    nodes <- tibble::tibble(ID = list_element) %>%
+    list_element <- data$CaNSample$CaNmod$components_param$Component #Get the ecosystem components
+    
+    existing_images<-list.files("inst/app/www/img")
+
+    nodes <- tibble::tibble(ID = list_element) %>% #Create the nodes of the foodweb network
       left_join(Info_table) %>%
       mutate(
-        id = ID,
-        label = case_when(
+        id = ID, #Give them an ID
+        label = case_when( #Show their ID as label only when the user chose to show the labels
           input$show_node_labels ~ id,
           TRUE ~ ""
         ),
-        shape = "image",
+        shape = case_when(
+          paste0(id,".png") %in% existing_images ~ "image",
+          TRUE ~ "dot"
+        ),
         image = sprintf("www/img/%s.png", id),
         x = data$CaNSample$CaNmod$components_param$X * 1000,
         y = data$CaNSample$CaNmod$components_param$Y * 1000,
@@ -142,6 +140,23 @@ app_server <- function(input, output, session) {
         stabilizationIterationsDone = "function() { this.physics.physicsEnabled = false; }"
       )
   })
+  
+  #Put the positions of the nodes into a reactive values to save them later
+  Positions <- reactiveValues(x = NULL, y = NULL)
+  
+  #Assign the initial positions to the ones from CaNSample
+  observe({
+    Positions$x <- data$CaNSample$CaNmod$components_param$X
+    Positions$y <- data$CaNSample$CaNmod$components_param$Y
+  })
+  
+  #If we move a node, assign the new node position to the reactive value
+  observeEvent(input$node_positions, {
+    Positions$x <- sapply(input$node_positions, `[[`, "x") / 1000
+    Positions$y <- sapply(input$node_positions, `[[`, "y") / 1000
+  })
+  
+
   
   observeEvent(input$selected_type, {
     if (input$selected_type == "node") {
